10644c91a9a0ed25f6e6a7b6605170e1fcc49329
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-06-21 14:19:58 -0600

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, PortafolioComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PortafolioComponent, AboutComponent, ItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
