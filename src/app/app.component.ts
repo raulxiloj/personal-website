63f2d2bd7f32775cf150776069f3ff1a882f7439
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-06-21 15:38:05 -0600

    [FEATURE]: Using service to present data in the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataPageService } from './services/data-page.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portafolio';
+
+  constructor(public dataPageService: DataPageService) {
+
+  }
+
 }
