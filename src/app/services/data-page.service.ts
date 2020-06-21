cbe5a89673ce605a26cc370c0356cd9e549ed799
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-06-21 15:56:48 -0600

    [ADD]: some animations added

--- src/app/services/data-page.service.ts
+++ src/app/services/data-page.service.ts
@@ -11,12 +11,10 @@ export class DataPageService {
   loaded: boolean = false;
 
   constructor(private http: HttpClient) {
-    console.log("Service of data page loaded")
     this.http.get('assets/data/page-data.json').subscribe(
       (res: dataPage) => {
         this.loaded = true;
         this.data = res;
-        console.log(this.data);
       }
     )
   }
