a4a4560e0339510e9d1d4045f094ac285e1bf8cc
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-06-22 04:15:34 -0600

    [ADD]: Search page created

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -3,24 +3,18 @@ import { ActivatedRoute } from '@angular/router';
 import { ProjectsService } from 'src/app/services/projects.service';
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
-export class ItemComponent implements OnInit {
-
-  project: any = {};
+export class SearchComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, public projectService: ProjectsService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       params => {
-        this.projectService.getProject(params['index']).subscribe(
-          project => {
-            this.project = project;
-          }
-        )
+        this.projectService.searchProject(params['tech']);
       }
     )
   }
